e5b1792a1b317d23d30534501c3c61e821b7867f
Usheninte Dangana <ninte@nullnow.com>
2024-08-16 15:35:23 +0100

    Added array of shopping cart items.

---
+++ cart.js
@@ -0,0 +1,7 @@
+const clothingItems = [
+  { name: 'Blue Jeans', price: 59.99, quantity: 5 },
+  { name: 'Red T-shirt', price: 19.99, quantity: 10 },
+  { name: 'Black Hoodie', price: 39.99, quantity: 8 },
+  { name: 'White Sneakers', price: 79.99, quantity: 4 },
+  { name: 'Sunglasses', price: 14.99, quantity: 20 },
+];
